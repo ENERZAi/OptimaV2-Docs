af79f59b1a843ce1b061c93fea55876570bfdc79
jwkim98 <jwkimrhkgkr@gmail.com>
2023-02-15 11:21:41 +0900

    Deployed dc75393 with MkDocs version: 1.4.2

--- template/sync-tabs.js
+++ template/sync-tabs.js
@@ -1,14 +1,15 @@
-(function () {
-    $(".tabbed-set > input").each((index, tab) => {
-        const id = tab.id;
-        const current = $(`label[for=${id}]`);
-        const name = current.text();
-        const labels = $(`.tabbed-set > label:contains(${name}), .tabbed-alternate > .tabbed-labels > label:contains(${name})`);
+(function() {
+$('.tabbed-set > input').each((index, tab) => {
+  const id = tab.id;
+  const current = $(`label[for=${id}]`);
+  const name = current.text();
+  const labels = $(`.tabbed-set > label:contains(${
+      name}), .tabbed-alternate > .tabbed-labels > label:contains(${name})`);
 
-        $(tab).click(() => {
-            labels.each((index, o) => {
-                $(`input[id=${$(o).attr("for")}]`).prop("checked", true);
-            });
-        });
+  $(tab).click(() => {
+    labels.each((index, o) => {
+      $(`input[id=${$(o).attr('for')}]`).prop('checked', true);
     });
+  });
+});
 })();
