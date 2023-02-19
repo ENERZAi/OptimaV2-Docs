22a043037b1673290e90e4f20ba5501a0644589a
jinhwan.shin <jinhwan.shin@enerzai.com>
2023-02-19 19:41:28 +0900

    Fixed moving page when switch between tabs

--- docs/template/sync-tabs.js
+++ docs/template/sync-tabs.js
@@ -1,14 +1,19 @@
 (function() {
-$('.tabbed-set > input').each((index, tab) => {
+$('.tabbed-set > input').each((_, tab) => {
   const id = tab.id;
   const current = $(`label[for=${id}]`);
   const name = current.text();
   const labels = $(`.tabbed-set > label:contains("${name}"), .tabbed-alternate > .tabbed-labels > label:contains("${name}")`);
 
   $(tab).click(() => {
-    labels.each((index, o) => {
+    const pos = current.offset().top;
+
+    labels.each((_, o) => {
       $(`input[id=${$(o).attr('for')}]`).prop('checked', true);
     });
+
+    const delta = current.offset().top - pos;
+    window.scrollBy(0, delta);
   });
 });
 })();
